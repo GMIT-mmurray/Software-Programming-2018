c97eb70f536400989012e39acad166da54ed9cab
mac <michael.murray@gmit.ie>
2018-09-18 07:58:52 +0100

    Mon 17th

---
+++ GroupA/sketch_sep17b/sketch_sep17b.ino
@@ -0,0 +1,35 @@
+const int kPinLed =   13;
+const int kPinLed12 = 12;
+void setup()
+{
+   pinMode(kPinLed, OUTPUT);
+   pinMode(kPinLed12, OUTPUT);
+   Serial.begin(9600);
+}
+
+int delayTime = 300;
+int delayTime1 = 700;
+
+void loop()
+{
+    Serial.println("Starting loop"); 
+    delayTime = delayTime - 30;
+    if(delayTime <= 0){   // If the delay time is zero or less, reset it.
+      delayTime = 300;
+    }
+    Serial.println("Pin 13");
+    digitalWrite(kPinLed, HIGH);
+    delay(delayTime);
+    digitalWrite(kPinLed, LOW);
+    delay(delayTime);
+
+    delayTime1 = delayTime1 - 40;
+    if (delayTime1 <= 0) {
+      delayTime1 = 600;
+    }
+    Serial.println("Pin 12");
+    digitalWrite(kPinLed12, HIGH);
+    delay(delayTime1);
+    digitalWrite(kPinLed12, LOW);
+    delay(delayTime1);    
+}
---
+++ GroupB/sketch_sep17a/sketch_sep17a.ino
@@ -0,0 +1,35 @@
+const int kPinLed = 13;
+const int kPin12 = 12;
+void setup()
+{
+  pinMode(kPinLed, OUTPUT);
+  pinMode(kPin12, OUTPUT);
+  Serial.begin(9600);
+}
+
+int delayTime = 1000;
+int delayTime12 = 2000;
+void loop()
+{
+  Serial.println("Starting Loop");
+  delayTime = delayTime - 101;
+  delayTime12 = delayTime12 - 500;
+
+  if (delayTime <= 0) { // If the delay time is zero or less, reset it.
+    delayTime = 1000;
+  }
+  Serial.print("Delay Time for 13 is ");
+  Serial.println(delayTime);
+  digitalWrite(kPinLed, HIGH);
+  delay(delayTime);
+  digitalWrite(kPinLed, LOW);
+  delay(delayTime);
+
+  if (delayTime12 <= 0) {
+    delayTime12 = 2000;
+  }
+  digitalWrite(kPin12, HIGH);
+  delay(delayTime12);
+  digitalWrite(kPin12, LOW);
+  delay(delayTime12);
+}
