e2f24d8f036acb768a1f9371eda3b9ca389af944
mac <michael.murray@gmit.ie>
2018-10-05 06:41:02 +0100

    1.10.18

---
+++ GroupB/sketch_oct01a/sketch_oct01a.ino
@@ -0,0 +1,32 @@
+const int kPinButton1 = 2;
+const int kPinLed = 9;
+const int kPinButton2 = 3;
+const int kPinLed1 = 10;
+void setup()
+{
+  pinMode(kPinButton1, INPUT);
+  pinMode(kPinButton2, INPUT);
+  //  digitalWrite(kPinButton1, HIGH); // turn on pull-up resistor
+  pinMode(kPinLed, OUTPUT);
+  pinMode(kPinLed1, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(kPinButton1) == LOW) {
+    digitalWrite(kPinLed, LOW);
+  }
+  else {
+    digitalWrite(kPinLed, HIGH);
+  }
+
+  int value = digitalRead(kPinButton2);
+  if (value == LOW) {
+    digitalWrite(kPinLed1, LOW);
+  }
+  else {
+    digitalWrite(kPinLed1, HIGH);
+  }
+
+}
+
---
+++ GroupB/sketch_oct01b/sketch_oct01b.ino
@@ -0,0 +1,27 @@
+const int kPinStart = 2;
+const int kPinLedStart = 8;
+const int kPinEnd = 7;
+const int kPinLedEnd = 13;
+const int diff = kPinLedStart-kPinStart;
+void setup()
+{
+  for (int i=kPinStart; i <= kPinEnd; i++) {
+    pinMode(i, INPUT);
+  }
+  for (int i=kPinLedStart; i <= kPinLedEnd; i++) {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void loop()
+{
+  for (int i=kPinStart; i<=kPinEnd; i++) {
+    if (digitalRead(i) == LOW) {
+      digitalWrite(i+diff, LOW);
+    }
+    else {
+      digitalWrite(i+diff, HIGH);
+    }
+  }
+}
+
