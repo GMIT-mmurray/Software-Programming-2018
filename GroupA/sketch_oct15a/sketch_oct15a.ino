aeed5a8efe57a6018754608e07113dea0417c1ad
mac <michael.murray@gmit.ie>
2018-10-16 08:26:19 +0100

    15.10.18

---
+++ GroupA/sketch_oct15a/sketch_oct15a.ino
@@ -0,0 +1,21 @@
+const int kPinLed = 13;
+void setup()
+{
+  pinMode(kPinLed, OUTPUT);
+   Serial.begin(9600);
+}
+
+int delayTime = 1000;
+void loop()
+{
+    delayTime = delayTime - 100;
+    if(delayTime <= 0){   // If it would have been zero or less, reset it.
+      delayTime = 1000;
+    }
+    Serial.print("delayTime = ");
+    Serial.println(delayTime);
+    digitalWrite(kPinLed, HIGH);
+    delay(delayTime);
+    digitalWrite(kPinLed, LOW);
+    delay(delayTime);
+}
